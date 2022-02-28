be8a1ecaf97460a8d254fa672d5da5cf6eeec0ec
Troels Peter Jessen <trojes@tuta.io>
2022-02-28 11:09:46 +0100

    Cleaned up code

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,42 @@ pub struct Match3Plugin;
 
 impl Plugin for Match3Plugin {
     fn build(&self, app: &mut App) {
-        let (dimensions, types) = app.world.get_resource::<Match3Config>().map_or(
-            (UVec2::new(10, 10), 5),
-            |Match3Config {
-                 gem_types,
-                 board_dimensions,
-             }| (*board_dimensions, *gem_types),
-        );
+        let Match3Config {
+            board_dimensions,
+            gem_types,
+        } = app
+            .world
+            .get_resource::<Match3Config>()
+            .copied()
+            .unwrap_or_default();
 
         let mut gems = HashMap::default();
-        (0..dimensions.x).for_each(|x| {
-            (0..dimensions.y).for_each(|y| {
-                gems.insert(UVec2::new(x, y), rand::thread_rng().gen_range(0..types));
+        (0..board_dimensions.x).for_each(|x| {
+            (0..board_dimensions.y).for_each(|y| {
+                gems.insert(UVec2::new(x, y), rand::thread_rng().gen_range(0..gem_types));
             })
         });
 
-        app.insert_resource(Board { dimensions, gems });
+        app.insert_resource(Board {
+            dimensions: board_dimensions,
+            gems,
+        });
     }
 }
 
+#[derive(Clone, Copy)]
 pub struct Match3Config {
     pub gem_types: u32,
     pub board_dimensions: UVec2,
+}
+
+impl Default for Match3Config {
+    fn default() -> Self {
+        Self {
+            gem_types: 5,
+            board_dimensions: UVec2::new(10, 10),
+        }
+    }
 }
 
 pub struct Board {
@@ -39,12 +53,14 @@ impl std::fmt::Display for Board {
         let res = (0..self.dimensions.x).map(|x| {
             f.write_fmt(format_args!(
                 "{:?}\n",
-                (0..self.dimensions.y).map(|y| self.gems[&UVec2::new(x, y)]).collect::<Vec<_>>()
+                (0..self.dimensions.y)
+                    .map(|y| self.gems[&UVec2::new(x, y)])
+                    .collect::<Vec<_>>()
             ))
         });
         for res in res {
             match res {
-                Ok(_) => {},
+                Ok(_) => {}
                 err => return err,
             }
         }
